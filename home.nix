f8723a0d2af5beb82c9c8cd0cc853c9357e64d22
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-01-20 11:52:32 +1300

    fix typo in git username

--- home.nix
+++ home.nix
@@ -80,7 +80,7 @@
   programs.git = {
     enable = true;
     userName = "Scott McKendry";
-    userEmail = "9483124+scottmckendry@users.noreply.github.com";
+    userEmail = "39483124+scottmckendry@users.noreply.github.com";
     aliases = {
       co = "checkout";
       hist = "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short";

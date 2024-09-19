9a1762574de6ba431109bfe02e28a9a9bdaebbcf
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-09-19 20:33:00 +1200

    use declaritive symlinks instead of setup script

--- home.nix
+++ home.nix
@@ -1,12 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
+let
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+  nixDir = "${config.home.homeDirectory}/git/nix";
+in
 {
-  imports = [ ];
+  imports = [ ./modules/home/lazygit.nix ];
 
-  programs.zoxide.enable = true;
+  # symlinks
+  xdg.configFile."bat".source = mkOutOfStoreSymlink "${nixDir}/bat";
+  xdg.configFile."fastfetch".source = mkOutOfStoreSymlink "${nixDir}/fastfetch";
+  xdg.configFile."nvim".source = mkOutOfStoreSymlink "${nixDir}/nvim";
+  xdg.configFile."starship".source = mkOutOfStoreSymlink "${nixDir}/starship";
+
+  programs.fastfetch.enable = true;
   programs.gh.enable = true;
-  programs.lazygit.enable = true;
   programs.starship.enable = true;
+  programs.zoxide.enable = true;
 
   nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
@@ -16,7 +26,6 @@
     discord
     dolphin
     dunst
-    fastfetch
     fd
     firefox
     fzf
@@ -33,19 +42,18 @@
     spotify
     sqlite
     unzip
+    wezterm
     wget
     wl-clipboard
-    wezterm
     zig
   ];
 
   programs.bash = {
     enable = true;
-
     shellAliases = {
       cd = "z";
       cdi = "zi";
-
+      cat = "bat";
       rebuild = "nh os switch $HOME/git/nix";
     };
   };
@@ -57,6 +65,13 @@
     aliases = {
       co = "checkout";
       hist = ''log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'';
+    };
+    delta = {
+      enable = true;
+      options = {
+        side-by-side = true;
+        syntax-theme = "cyberdream";
+      };
     };
   };
 
@@ -76,6 +91,12 @@
       };
     };
   };
+
+  # bat setup
+  home.activation.batSetup = config.lib.dag.entryBefore [ "writeBoundary" ] ''
+    ${pkgs.bat}/bin/bat cache --clear
+    ${pkgs.bat}/bin/bat cache --build
+  '';
 
   # Do not modify state version
   home.stateVersion = "24.05";
---
+++ modules/home/lazygit.nix
@@ -0,0 +1,17 @@
+{ pkgs, config, ... }:
+
+let
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+  nixDir = "${config.home.homeDirectory}/git/nix";
+in
+{
+  xdg.configFile."lazygit".source = mkOutOfStoreSymlink "${nixDir}/lazygit";
+  home.packages = with pkgs; [
+    lazygit
+    delta
+  ];
+
+  stylix.targets = {
+    lazygit.enable = false;
+  };
+}

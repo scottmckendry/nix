ab3b5d724e730ae699ab132099aeb66b6b7f3ead
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-09-21 09:31:27 +1200

    use combined .net sdks

--- modules/work.nix
+++ modules/work.nix
@@ -1,12 +1,26 @@
 { pkgs, ... }:
+
+let
+  dotnet-combined = (
+    with pkgs.dotnetCorePackages;
+    combinePackages [
+      sdk_8_0
+      sdk_6_0
+    ]
+  );
+in
 {
   services.twingate.enable = true;
   environment.systemPackages = with pkgs; [
     azure-cli
     bicep
     dbeaver-bin
-    dotnet-sdk_8
+    dotnet-combined
     remmina
     teams-for-linux
   ];
+
+  environment.sessionVariables = {
+    DOTNET_ROOT = "${dotnet-combined}";
+  };
 }

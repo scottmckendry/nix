1871c79a14db97a4311f77a6e4fbdcafb8ecadad
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-01-31 14:17:51 +1300

    add klassy plasma package

--- modules/plasma.nix
+++ modules/plasma.nix
@@ -5,7 +5,8 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  environment.systemPackages = [
+  environment.systemPackages = with pkgs; [
     inputs.kwin-effects-forceblur.packages.${pkgs.system}.default
+    nur.repos.shadowrz.klassy-qt6
   ];
 }

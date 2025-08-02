977e24a5754e2e5af47f04f6390b7e9a1baaec74
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-08-02 20:07:12 +1200

    consistent cursors

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -8,7 +8,7 @@
   home.pointerCursor = {
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Classic";
-    size = 24;
+    size = 16;
     gtk.enable = true;
     x11.enable = true;
   };

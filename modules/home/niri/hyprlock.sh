c3fcaafbc48adda165930355fc1e479c8c849c5e
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-08-06 19:19:28 +1200

    close gnome keyring prompt on first login

--- modules/home/niri/hyprlock.sh
+++ modules/home/niri/hyprlock.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Login wrapper for hyprlock. Since calling hyprlock directly on startup seems to fail consistently
+# Login wrapper for hyprlock. Since calling hyprlock directly on startup seems to fail consistently.
 # ¯\_(ツ)_/¯
 
 LOGFILE="$HOME/.cache/hyprlock.log"
@@ -15,5 +15,11 @@ STATUS=$?
 if [ $STATUS -ne 0 ]; then
     notify-send "Hyprlock failed :(" "Exit code: $STATUS. See $LOGFILE"
 fi
+
+# HACK: automatically close the focussed window right after unlocking
+# This window happens to be the Gnome Keyring unlock dialog which
+# is (after unlocking with hyprlock) redundant. For this reason, this
+# script should only be run at login, not for normal locking/idle.
+niri msg action close-window
 
 exit $STATUS

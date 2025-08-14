4fc1159d7aa03fafaa50de80b965b1a2a6bc80e0
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-08-14 19:01:16 +1200

    remove walker plugins

--- modules/home/walker/default.nix
+++ modules/home/walker/default.nix
@@ -33,15 +33,4 @@ in
   ];
 
   xdg.configFile."walker/themes".source = mkOutOfStoreSymlink "${nixDir}/modules/home/walker/themes";
-
-  # Create stubs for custom plugins.
-  home.activation.walkerPlugins = ''
-    rm -rf ~/.config/walker/plugins
-    mkdir -p ~/.config/walker/plugins
-    for plugin in ~/git/nix/modules/home/walker/plugins/*.lua; do
-      name=$(basename "$plugin")
-      echo "return dofile(\"$plugin\")" > ~/.config/walker/plugins/"$name"
-    done
-    chmod -R 755 ~/.config/walker/plugins
-  '';
 }

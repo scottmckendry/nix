5052389a85006c217fc4eda5676bb37af166aa8b
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2025-09-02 07:57:24 +1200

    remove now redundant env var from walker service

--- modules/home/niri/services/walker.nix
+++ modules/home/niri/services/walker.nix
@@ -10,7 +10,6 @@
 
     Service = {
       Type = "simple";
-      Environment = "DISPLAY=:0"; # for xwayland apps
       ExecStart = "${pkgs.walker}/bin/walker --gapplication-service";
       Restart = "on-failure";
       RestartSec = 1;

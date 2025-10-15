fb94430c0c31ee8329e7acac925584136020ad55
Scott McKendry <me@scottmckendry.tech>
2025-10-15 10:42:12 +0700

    automatic timezone detection

--- modules/locale.nix
+++ modules/locale.nix
@@ -1,21 +1,14 @@
-{ config, pkgs, ... }:
+{ ... }:
 
-let
-  defaultLocale = "en_NZ.UTF-8";
-in
 {
-  time.timeZone = "Pacific/Auckland";
-  i18n.defaultLocale = "en_GB.UTF-8";
+  services.automatic-timezoned.enable = true;
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = defaultLocale;
-    LC_IDENTIFICATION = defaultLocale;
-    LC_MEASUREMENT = defaultLocale;
-    LC_MONETARY = defaultLocale;
-    LC_NAME = defaultLocale;
-    LC_NUMERIC = defaultLocale;
-    LC_PAPER = defaultLocale;
-    LC_TELEPHONE = defaultLocale;
-    LC_TIME = defaultLocale;
+  i18n = {
+    defaultLocale = "en_NZ.UTF-8";
+    supportedLocales = [
+      "en_NZ.UTF-8/UTF-8"
+      "en_GB.UTF-8/UTF-8"
+    ];
   };
+
 }

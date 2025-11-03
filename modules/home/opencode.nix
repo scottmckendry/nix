edbb935d6e19cc1c7b94cf851eebe72ff7eb54bb
Scott McKendry <me@scottmckendry.tech>
2025-11-04 09:32:26 +1300

    pin opencode to pre-1.0 nixpgks rev

--- modules/home/opencode.nix
+++ modules/home/opencode.nix
@@ -1,7 +1,9 @@
-{ pkgs, ... }:
-
+{ inputs, ... }:
+let
+  pinnedPackage = inputs.nixpkgs-opencode.legacyPackages.${"x86_64-linux"}.opencode;
+in
 {
-  home.packages = [ pkgs.opencode ];
+  home.packages = [ pinnedPackage ];
   home.file.".config/opencode/config.json".text = ''
     {
       "$schema": "https://opencode.ai/config.json",

5222b7c7a751924776959c3a9464e8fac52e2800
Scott McKendry <me@scottmckendry.tech>
2025-12-01 07:56:32 +1300

    enable powertop, show charge time in waybar tooltip

--- hosts/eris/power-management.nix
+++ hosts/eris/power-management.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # Enable deep sleep mode with hibernation support - requires swap space
@@ -13,6 +13,8 @@
   boot.kernelParams = [ "resume_offset=36454400" ];
   boot.resumeDevice = "/dev/disk/by-uuid/1307e52a-da90-4c68-9a88-1180e1bcdd0f";
   powerManagement.enable = true;
+  powerManagement.powertop.enable = true;
+  environment.systemPackages = [ pkgs.powertop ];
 
   services.power-profiles-daemon.enable = true;
 

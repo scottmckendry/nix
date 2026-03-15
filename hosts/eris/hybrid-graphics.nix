b89be6a926b680cd7070777fdc30cd0e3cc9b7a3
Scott McKendry <me@scottmckendry.tech>
2026-03-16 04:53:26 +1300

    nvidia: enable fine-grained power management for eris

--- hosts/eris/hybrid-graphics.nix
+++ hosts/eris/hybrid-graphics.nix
@@ -1,14 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   custom.desktop.nvidia.enable = true;
-  hardware.nvidia.prime = {
-    offload = {
-      enable = true;
-      enableOffloadCmd = true;
-    };
+  hardware.nvidia = {
+    package = pkgs.linuxPackages.nvidiaPackages.beta;
+    powerManagement.finegrained = true;
+    powerManagement.kernelSuspendNotifier = true;
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
 
-    intelBusId = "PCI:00:02:0";
-    nvidiaBusId = "PCI:243:00:0";
+      intelBusId = "PCI:00:02:0";
+      nvidiaBusId = "PCI:243:00:0";
+    };
   };
 }
--- modules/nixos/desktop/nvidia.nix
+++ modules/nixos/desktop/nvidia.nix
@@ -15,5 +15,6 @@ in
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.graphics.enable = true;
     hardware.nvidia.open = true;
+    hardware.nvidia.powerManagement.enable = true;
   };
 }

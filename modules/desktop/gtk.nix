8c20388218ac00638a4201a547ae45513e1b004b
Scott McKendry <me@scottmckendry.tech>
2026-04-27 19:47:40 +1200

    colloid gtk theme

--- modules/desktop/gtk.nix
+++ modules/desktop/gtk.nix
@@ -6,27 +6,28 @@
     nixos =
       { pkgs, ... }:
       let
-        nightfox = pkgs.nightfox-gtk-theme.override {
-          colorVariants = [ "dark" ];
-          tweakVariants = [ "carbonfox" ];
+        colloid = pkgs.colloid-gtk-theme.override {
+          themeVariants = [ "purple" ];
+          tweaks = [ "nord" ];
         };
+        theme = "Colloid-Purple-Dark-Nord";
       in
       {
         environment.systemPackages = with pkgs; [
           adwaita-icon-theme
           bibata-cursors
-          nightfox
+          colloid
           tela-icon-theme
         ];
 
-        environment.sessionVariables.GTK_THEME = "Nightfox-Dark-Carbonfox";
+        environment.sessionVariables.GTK_THEME = theme;
         environment.sessionVariables.GTK_USE_PORTAL = "1";
         environment.sessionVariables.XCURSOR_SIZE = "16";
 
         programs.dconf.profiles.user.databases = [
           {
             settings."org/gnome/desktop/interface" = {
-              gtk-theme = "Nightfox-Dark-Carbonfox";
+              gtk-theme = theme;
               icon-theme = "Tela-dracula-dark";
               cursor-theme = "Bibata-Modern-Classic";
               cursor-size = lib.gvariant.mkInt32 16;

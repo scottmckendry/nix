015b914ff1da79f1ee031a580f5d600e9f1334bb
Scott McKendry <me@scottmckendry.tech>
2026-05-30 16:20:21 +1200

    update intune packages, enforce pwquality

--- modules/services/intune.nix
+++ modules/services/intune.nix
@@ -3,7 +3,7 @@
   # NOTE: Initial registration only seems to work on GNOME.
   den.aspects.intune = {
     nixos =
-      { pkgs, ... }:
+      { pkgs, config, ... }:
       {
         services.intune.enable = true;
 
@@ -11,6 +11,21 @@
 
         environment.sessionVariables = {
           SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
+        };
+
+        security.pam.services.passwd.rules.password = {
+          pwquality = {
+            control = "requisite";
+            modulePath = "${pkgs.libpwquality}/lib/security/pam_pwquality.so";
+            order = config.security.pam.services.passwd.rules.password.unix.order - 50;
+            settings = {
+              minlen = 16;
+              dcredit = -1; # at least 1 digit
+              ucredit = -1; # at least 1 uppercase
+              lcredit = -1; # at least 1 lowercase
+              ocredit = -1; # at least 1 symbol
+            };
+          };
         };
       };
   };

00b2465a934cdb5d638883d38a44a831358e7e8d
Scott McKendry <me@scottmckendry.tech>
2026-09-11 20:00:32 +1200

    system update

--- modules/desktop/displaylink.nix
+++ modules/desktop/displaylink.nix
@@ -2,11 +2,9 @@
 {
   den.aspects.displaylink = {
     nixos =
-      { pkgs, config, lib, ... }:
+      { pkgs, config, ... }:
       {
         boot = {
-          # TODO: evdi broken on 7.2; revert to linuxPackages_latest when fixed upstream
-          kernelPackages = lib.mkForce pkgs.linuxPackages_7_1;
           extraModulePackages = [ config.boot.kernelPackages.evdi ];
           initrd.kernelModules = [ "evdi" ];
         };

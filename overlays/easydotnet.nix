1c0095deb5c3f81be58db7e19c0ab2a5792e7c62
Scott McKendry <me@scottmckendry.tech>
2026-09-13 16:48:28 +1200

    bump easydotnet

--- overlays/easydotnet.nix
+++ overlays/easydotnet.nix
@@ -2,8 +2,8 @@ final: prev: {
   easydotnet = prev.buildDotnetGlobalTool {
     pname = "dotnet-easydotnet";
     nugetName = "EasyDotnet";
-    version = "3.2.2";
-    nugetHash = "sha256-EX8RnhRUVwm/LPOob1t6407j0SNpAErgZ3QMv0KDJCI=";
+    version = "3.4.25";
+    nugetHash = "sha256-RburiBDwtkNcTmzce2HHY1HJWdoOFx9x692PBd2VD9Y=";
     dotnet-sdk = prev.dotnetCorePackages.sdk_8_0;
   };
 }
